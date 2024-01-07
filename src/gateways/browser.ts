f16c7826f758db95189281101f4fa695242c30d7
mateus birtan <contatobirtan@gmail.com>
2024-01-07 13:45:13 -0300

    Update puppeteer version and set headless mode to "new"

--- src/gateways/browser.ts
+++ src/gateways/browser.ts
@@ -4,7 +4,7 @@ import stealthPlugin from "puppeteer-extra-plugin-stealth";
 export async function initializeBrowser() {
   puppeteerExtra.use(stealthPlugin());
   const browser = await puppeteerExtra.launch({
-    headless: false,
+    headless: "new",
     executablePath: '/usr/bin/chromium-browser',
   });
   return browser;
